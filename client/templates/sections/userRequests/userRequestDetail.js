3c3f336c3bdb5b882534ee571b8c50dfa725acbe
Riccardo Maggi <rmaggi@expedia.com>
2014-12-28 19:10:11 +0100

    missing from last commit

---
+++ client/templates/sections/userRequests/userRequestDetail.js
@@ -0,0 +1,12 @@
+Template.userRequestDetail.helpers({
+
+    getLabelFromRequestStatus: function(status){
+        return com.pigeon.util.getLabelFromRequestStatus(status);
+    },
+
+    //TODO: this must go into some logic used in router (if expired...)
+    getRequestLifeTime: function(creationTime){
+        return (creationTime + 172800000);
+    }
+
+});
